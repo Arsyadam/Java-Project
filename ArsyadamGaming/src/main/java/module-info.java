66c824c7aa4283e0c2b11ff9d54ef6a911fd6e92
Arsyadam <work@arsyadam.id>
2024-01-30 13:23:17 +0700

    Tugas Pak Son constructur

--- ArsyadamGaming/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.arsyadamgaming {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.arsyadamgaming to javafx.fxml;
-    exports com.example.arsyadamgaming;
-}
---
+++ User/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person dika = new Person();
+
+        dika.setUsername("Arsyadam");
+        dika.setPassword("05Arsyad06Ali2008Mahardika");
+
+        System.out.println("Username: " + dika.getUsername());
+        System.out.println("Password: " + dika.getPassword());
+    }
+
+}
